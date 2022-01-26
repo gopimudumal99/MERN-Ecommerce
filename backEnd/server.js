7bbe40c24dc1c965dd958208788a42f6119c56b3
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-01-26 09:39:01 +0530

    Handling uncaught exeception errors

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,19 +3,33 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./configs/db");
 const errorHander = require("./middleware/error");
 
+
+// Handling UnCaught Exeception
+process.on("uncaughtException", (err) => { 
+    console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to UnCaught Exception `);
+  process.exit(1)
+})
+
+
 // config
 dotenv.config({ path: "backEnd/configs/config.env" });
+
 
 //Connecting to Database
 connectDatabase();
 
+
 //middleware Error hander
 app.use(errorHander);
+
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Listing on port http://localhost:${process.env.PORT}`);
 });
 
+
+console.log(product)
 // Unhandle Promise Rejections
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
